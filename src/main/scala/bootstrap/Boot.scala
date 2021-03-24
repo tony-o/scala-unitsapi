f08108cfb6cf173d8b608dd2e743c5189744ac4a
tony-o <tony.odell@live.com>
2021-03-24 10:43:59 -0700

    return a 422 if string contains invalid info

--- src/main/scala/bootstrap/Boot.scala
+++ src/main/scala/bootstrap/Boot.scala
@@ -4,19 +4,23 @@ import net.liftweb.http.LiftRules
 import net.liftweb.http._
 import net.liftweb.http.rest._
 import net.liftweb.json._
-import net.liftweb.common.{Full,Empty}
+import net.liftweb.common.{Full,Empty,Failure,Box}
 import com.tonyo.Calculator
 
 object API extends RestHelper {
+
+  /* allow us to return a well formed error if str is invalid */
+  def boxify(str : String) : Box[(Double, String)] = Calculator.lrParse(str) match {
+    case Right(t) => Full(t)
+    case Left(s)  => Failure(JsonAST.compactRender(JObject(List(JField("error", JString(s))))))
+  }
+
   serve {
     case Req("units" :: "si" :: Nil, _, req) => for {
         unit_s <- S.param("units") ?~ "{\"error\": \"'units' in query string required\"}" ~> 400
-        calc_t = Calculator.lrParse(unit_s)
-      } yield calc_t match {
-        case Right(t) => JObject(List(JField("multiplication_factor", JDouble(t._1)),
-                                      JField("unit_name",             JString(t._2)) ))
-        case Left(s)  => JObject(List(JField("error", JString(s)))) /* this returns a 200, shouldn't */
-      }
+        calc_t <- boxify(unit_s) ~> 422
+      } yield JObject(List(JField("multiplication_factor", JDouble(calc_t._1)),
+                           JField("unit_name",             JString(calc_t._2)) ))
   }
 }
 
